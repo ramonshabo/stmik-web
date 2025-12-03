df65be491395e89c4b886d5f173e8818fa8232b3
Ramon Shabo <ramonwin@programmer.net>
2025-12-03 09:07:20 +0700

    rev

--- session/koneksi.php
+++ session/koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";       // sesuaikan dengan user MySQL kalian
-$pass = "";           // isi password MySQL kalau ada
+$pass = "root";           // isi password MySQL kalau ada
 $db   = "db_stmikweb";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
