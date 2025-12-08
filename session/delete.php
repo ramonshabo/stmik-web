fad224face89d161d613ac46c70bbdae6427ad64
Ramon Shabo <ramonwin@programmer.net>
2025-12-08 09:00:28 +0700

    rev

--- session/delete.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include "koneksi.php";
-
-$id = $_GET['id'];
-$query = mysqli_query($conn, "DELETE FROM barang where id= $id");
-
-header("Location:dashboard.php");
-
-?>
--- session/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // hapus semua session
-header("Location: login.php");
-exit;
-?>
